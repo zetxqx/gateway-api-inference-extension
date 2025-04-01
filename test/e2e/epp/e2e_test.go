110f490bdf2dd55230e5c387d8de091c406e5d61
Kellen Swain <kfswain@google.com>
2025-04-01 12:50:46 -0700

    Update e2e test config (#636)

--- test/e2e/epp/e2e_test.go
+++ test/e2e/epp/e2e_test.go
@@ -94,11 +94,11 @@ var _ = ginkgo.Describe("InferencePool", func() {
 func newInferenceModel(ns string) *v1alpha2.InferenceModel {
 	targets := []v1alpha2.TargetModel{
 		{
-			Name:   modelName + "-0",
+			Name:   modelName,
 			Weight: ptr.To(int32(50)),
 		},
 		{
-			Name:   modelName + "-1",
+			Name:   "cad-fabricator",
 			Weight: ptr.To(int32(50)),
 		},
 	}
