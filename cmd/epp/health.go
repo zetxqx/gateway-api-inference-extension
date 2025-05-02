a305927da234c48e89f917313fdf48daba81827e
Shane Utt <shaneutt@linux.com>
2025-05-02 14:16:41 -0400

    fix: add required List() method to epp health server
    Signed-off-by: Shane Utt <shaneutt@linux.com>

--- cmd/epp/health.go
+++ cmd/epp/health.go
@@ -48,6 +48,20 @@ func (s *healthServer) Check(ctx context.Context, in *healthPb.HealthCheckReques
 	return &healthPb.HealthCheckResponse{Status: healthPb.HealthCheckResponse_SERVING}, nil
 }
 
+func (s *healthServer) List(ctx context.Context, _ *healthPb.HealthListRequest) (*healthPb.HealthListResponse, error) {
+	// currently only the ext_proc service is provided
+	serviceHealthResponse, err := s.Check(ctx, &healthPb.HealthCheckRequest{Service: extProcPb.ExternalProcessor_ServiceDesc.ServiceName})
+	if err != nil {
+		return nil, err
+	}
+
+	return &healthPb.HealthListResponse{
+		Statuses: map[string]*healthPb.HealthCheckResponse{
+			extProcPb.ExternalProcessor_ServiceDesc.ServiceName: serviceHealthResponse,
+		},
+	}, nil
+}
+
 func (s *healthServer) Watch(in *healthPb.HealthCheckRequest, srv healthPb.Health_WatchServer) error {
 	return status.Error(codes.Unimplemented, "Watch is not implemented")
 }
