62f226c325a96c65e2a96450f925ad74c159a36d
Nir Rozenbaum <nirro@il.ibm.com>
2025-05-11 16:47:14 +0300

    merge functions in env utils (#819)
    Signed-off-by: Nir Rozenbaum <nirro@il.ibm.com>

--- pkg/epp/util/env/env.go
+++ pkg/epp/util/env/env.go
@@ -10,43 +10,27 @@ import (
 	"github.com/go-logr/logr"
 )
 
-// parseEnvWithValue attempts to parse a string value using the provided
-// parser.
-// It logs success or failure and returns the parsed value or the default
-// value.
-// This helper is used when the environment variable is confirmed to exist.
-func parseEnvWithValue[T any](key string, valueStr string, defaultVal T,
-	parser func(string) (T, error), logger logr.Logger) T {
-	parsedVal, err := parser(valueStr)
-	if err != nil {
-		logger.Info(fmt.Sprintf("Failed to parse environment variable as %s, using default value", reflect.TypeOf(defaultVal)),
-			"key", key, "rawValue", valueStr, "error", err,
-			"defaultValue", defaultVal)
-		return defaultVal
-	}
-
-	logger.Info("Successfully loaded environment variable",
-		"key", key, "value", parsedVal)
-	return parsedVal
-}
-
-// getEnvWithParser retrieves an environment variable. If set, it uses the
-// provided parser to convert it. Otherwise, it returns the default value.
-// It delegates to parseEnvWithValue for the actual parsing and detailed
-// logging once the variable is confirmed to exist.
-func getEnvWithParser[T any](key string, defaultVal T,
-	parser func(string) (T, error), logger logr.Logger) T {
+// getEnvWithParser retrieves an environment variable. If set, it uses the provided parser to parse it.
+// It logs success or failure and returns the parsed value or the default value in case of a failure.
+func getEnvWithParser[T any](key string, defaultVal T, parser func(string) (T, error), logger logr.Logger) T {
 	valueStr, exists := os.LookupEnv(key)
 	if !exists {
-		logger.Info("Environment variable not set, using default value",
-			"key", key, "defaultValue", defaultVal)
+		logger.Info("Environment variable not set, using default value", "key", key, "defaultValue", defaultVal)
 		return defaultVal
 	}
-	return parseEnvWithValue(key, valueStr, defaultVal, parser, logger)
+
+	parsedValue, err := parser(valueStr)
+	if err != nil {
+		logger.Info(fmt.Sprintf("Failed to parse environment variable as %s, using default value", reflect.TypeOf(defaultVal)),
+			"key", key, "rawValue", valueStr, "error", err, "defaultValue", defaultVal)
+		return defaultVal
+	}
+
+	logger.Info("Successfully loaded environment variable", "key", key, "value", parsedValue)
+	return parsedValue
 }
 
-// GetEnvFloat gets a float64 from an environment variable with a default
-// value.
+// GetEnvFloat gets a float64 from an environment variable with a default value.
 func GetEnvFloat(key string, defaultVal float64, logger logr.Logger) float64 {
 	parser := func(s string) (float64, error) { return strconv.ParseFloat(s, 64) }
 	return getEnvWithParser(key, defaultVal, parser, logger)
@@ -57,14 +41,12 @@ func GetEnvInt(key string, defaultVal int, logger logr.Logger) int {
 	return getEnvWithParser(key, defaultVal, strconv.Atoi, logger)
 }
 
-// GetEnvDuration gets a time.Duration from an environment variable with a
-// default value.
+// GetEnvDuration gets a time.Duration from an environment variable with a default value.
 func GetEnvDuration(key string, defaultVal time.Duration, logger logr.Logger) time.Duration {
 	return getEnvWithParser(key, defaultVal, time.ParseDuration, logger)
 }
 
-// GetEnvString gets a string from an environment variable with a default
-// value.
+// GetEnvString gets a string from an environment variable with a default value.
 func GetEnvString(key string, defaultVal string, logger logr.Logger) string {
 	parser := func(s string) (string, error) { return s, nil }
 	return getEnvWithParser(key, defaultVal, parser, logger)
