68c73c0187e3f2e5802ad5f6a647c760df3416a9
sina chavoshi <chavoshi@google.com>
2025-06-17 13:40:51 -0700

    feat(conformance): Add HTTPRouteMultipleRulesDifferentPools test (#834)
    * copy of accepted inference pool test to start from.
    
    * add yaml file for the test
    
    * update time out
    
    * update the yaml file to add port 9002
    
    * read timeout config from local repo
    
    * remove excess comments
    
    * correct spelling for scenarios
    
    * check route condition on RouteConditionResolvedRefs
    
    * remove empty lines in yaml
    
    * set optional/defaulted fields as unspecified
    
    * fix timeout
    
    * fix boilerplate header
    
    * change varialbe names to use primary secondary consistently.
    
    * remove extra comments
    
    * factor out common code
    
    * Add actual http traffic validation using echo-basic
    
    * remove extra comments from manifest
    
    * remove modifiedTimeoutConfig.HTTPRouteMustHaveCondition per review comment.
    
    * intermediate update
    
    * fix the test run
    
    * factor out common code
    
    * move epp def to shared manifest
    
    * remove extra comments
    
    * revert back to two epps
    
    * add to do for epp image
    
    * switch to GeneralMustHaveConditionTimeout
    
    * undo gateway version changes
    
    * remove unused HTTPRouteMustHaveConditions
    
    * update doc string for GetPod
    
    * update docstring
    
    * Remove resource type from names in manifests.
    
    * remove type from name
    
    * remove health check
    
    * add todo for combining getpod methods

--- conformance/utils/config/timing.go
+++ conformance/utils/config/timing.go
@@ -29,8 +29,8 @@ type InferenceExtensionTimeoutConfig struct {
 	// All fields from gatewayconfig.TimeoutConfig will be available directly.
 	gatewayconfig.TimeoutConfig
 
-	// InferencePoolMustHaveConditionTimeout represents the maximum time to wait for an InferencePool to have a specific condition.
-	InferencePoolMustHaveConditionTimeout time.Duration
+	// GeneralMustHaveConditionTimeout represents the maximum time to wait for an InferencePool, HttpRoute or other assets to have a specific condition.
+	GeneralMustHaveConditionTimeout time.Duration
 
 	// InferencePoolMustHaveConditionInterval represents the polling interval for checking an InferencePool's condition.
 	InferencePoolMustHaveConditionInterval time.Duration
@@ -38,16 +38,15 @@ type InferenceExtensionTimeoutConfig struct {
 	// GatewayObjectPollInterval is the polling interval used when waiting for a Gateway object to appear.
 	GatewayObjectPollInterval time.Duration
 
-	// HTTPRouteDeletionReconciliationTimeout is the time to wait for controllers to reconcile
-	// state after an HTTPRoute is deleted, before checking dependent resources or traffic.
+	// HTTPRouteConditionTimeout represents the maximum time to wait for an HTTPRoute to have a specific condition.
 	HTTPRouteDeletionReconciliationTimeout time.Duration
 }
 
 // DefaultInferenceExtensionTimeoutConfig returns a new InferenceExtensionTimeoutConfig with default values.
 func DefaultInferenceExtensionTimeoutConfig() InferenceExtensionTimeoutConfig {
 	return InferenceExtensionTimeoutConfig{
-		TimeoutConfig:                          gatewayconfig.DefaultTimeoutConfig(), // Initialize embedded struct
-		InferencePoolMustHaveConditionTimeout:  300 * time.Second,
+		TimeoutConfig:                          gatewayconfig.DefaultTimeoutConfig(),
+		GeneralMustHaveConditionTimeout:        300 * time.Second,
 		InferencePoolMustHaveConditionInterval: 10 * time.Second,
 		GatewayObjectPollInterval:              5 * time.Second,
 		HTTPRouteDeletionReconciliationTimeout: 5 * time.Second,
