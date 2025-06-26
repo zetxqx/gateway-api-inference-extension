8eced9a2f5dba096288efadc9e738d1afcf6f09f
Bob Tian <bobzetian@google.com>
2025-06-26 01:54:18 +0000

    extend types.

--- api/v1alpha2/inferencepool_types.go
+++ api/v1alpha2/inferencepool_types.go
@@ -194,14 +194,14 @@ type PoolStatus struct {
 	Conditions []metav1.Condition `json:"conditions,omitempty"`
 }
 
-// InferencePoolConditionType is a type of condition for the InferencePool
+// InferencePoolConditionType is a type of condition for a inference pool.
 type InferencePoolConditionType string
 
-// InferencePoolReason is the reason for a given InferencePoolConditionType
+// InferencePoolReason is a reason for a inference pool condition.
 type InferencePoolReason string
 
 const (
-	// This condition indicates whether the InferencePool has been accepted or rejected
+	// This condition indicates whether the inference pool has been accepted or rejected
 	// by a Gateway, and why.
 	//
 	// Possible reasons for this condition to be True are:
@@ -210,52 +210,94 @@ const (
 	//
 	// Possible reasons for this condition to be False are:
 	//
-	// * "NotSupportedByGateway"
+	// * "NotAllowedByListeners"
+	// * "NoMatchingListenerHostname"
+	// * "NoMatchingParent"
+	// * "UnsupportedValue"
 	//
 	// Possible reasons for this condition to be Unknown are:
 	//
 	// * "Pending"
 	//
-	// Controllers MAY raise this condition with other reasons, but should
-	// prefer to use the reasons listed above to improve interoperability.
+	// Controllers may raise this condition with other reasons,
+	// but should prefer to use the reasons listed above to improve
+	// interoperability.
 	InferencePoolConditionAccepted InferencePoolConditionType = "Accepted"
 
 	// This reason is used with the "Accepted" condition when the InferencePool has been
 	// accepted by the Gateway.
 	InferencePoolReasonAccepted InferencePoolReason = "Accepted"
 
-	// This reason is used with the "Accepted" condition when the InferencePool
-	// has not been accepted by a Gateway because the Gateway does not support
-	// InferencePool as a backend.
-	InferencePoolReasonNotSupportedByGateway InferencePoolReason = "NotSupportedByGateway"
+	// This reason is used with the "Accepted" condition when the inference pool has not
+	// been accepted by a Gateway because the Gateway has no Listener whose
+	// allowedRoutes criteria permit the inference pool
+	InferencePoolReasonNotAllowedByListeners InferencePoolReason = "NotAllowedByListeners"
+
+	// This reason is used with the "Accepted" condition when the Gateway has no
+	// compatible Listeners whose Hostname matches the inference pool
+	InferencePoolReasonNoMatchingListenerHostname InferencePoolReason = "NoMatchingListenerHostname"
+
+	// This reason is used with the "Accepted" condition when there are
+	// no matching Parents. In the case of Gateways, this can occur when
+	// a InferencePool ParentRef specifies a Port and/or SectionName that does not
+	// match any Listeners in the Gateway.
+	InferencePoolReasonNoMatchingParent InferencePoolReason = "NoMatchingParent"
+
+	// This reason is used with the "Accepted" condition when a value for an Enum
+	// is not recognized.
+	InferencePoolReasonUnsupportedValue InferencePoolReason = "UnsupportedValue"
 
 	// This reason is used with the "Accepted" when a controller has not yet
-	// reconciled the InferencePool.
+	// reconciled the inference pool.
 	InferencePoolReasonPending InferencePoolReason = "Pending"
+
+	// This reason is used with the "Accepted" condition when there
+	// are incompatible filters present on a inference pool rule (for example if
+	// the URLRewrite and RequestRedirect are both present on an HTTPRoute).
+	InferencePoolReasonIncompatibleFilters InferencePoolReason = "IncompatibleFilters"
 )
 
 const (
 	// This condition indicates whether the controller was able to resolve all
 	// the object references for the InferencePool.
 	//
-	// Possible reasons for this condition to be true are:
+	// Possible reasons for this condition to be True are:
 	//
 	// * "ResolvedRefs"
 	//
 	// Possible reasons for this condition to be False are:
 	//
-	// * "InvalidExtensionRef"
+	// * "RefNotPermitted"
+	// * "InvalidKind"
+	// * "BackendNotFound"
+	// * "UnsupportedProtocol"
 	//
-	// Controllers MAY raise this condition with other reasons, but should
-	// prefer to use the reasons listed above to improve interoperability.
+	// Controllers may raise this condition with other reasons,
+	// but should prefer to use the reasons listed above to improve
+	// interoperability.
 	InferencePoolConditionResolvedRefs InferencePoolConditionType = "ResolvedRefs"
 
 	// This reason is used with the "ResolvedRefs" condition when the condition
 	// is true.
 	InferencePoolReasonResolvedRefs InferencePoolReason = "ResolvedRefs"
 
-	// This reason is used with the "ResolvedRefs" condition when the
-	// ExtensionRef is invalid in some way. This can include an unsupported kind
-	// or API group, or a reference to a resource that can not be found.
-	InferencePoolReasonInvalidExtensionRef InferencePoolReason = "InvalidExtensionRef"
+	// This reason is used with the "ResolvedRefs" condition when
+	// one of the Listener's InferencePools has a BackendRef to an object in
+	// another namespace, where the object in the other namespace does
+	// not have a ReferenceGrant explicitly allowing the reference.
+	InferencePoolReasonRefNotPermitted InferencePoolReason = "RefNotPermitted"
+
+	// This reason is used with the "ResolvedRefs" condition when
+	// one of the InferencePool's rules has a reference to an unknown or unsupported
+	// Group and/or Kind.
+	InferencePoolReasonInvalidKind InferencePoolReason = "InvalidKind"
+
+	// This reason is used with the "ResolvedRefs" condition when one of the
+	// InferencePool's rules has a reference to a resource that does not exist.
+	InferencePoolReasonBackendNotFound InferencePoolReason = "BackendNotFound"
+
+	// This reason is used with the "ResolvedRefs" condition when one of the
+	// InferencePool's rules has a reference to a resource with an app protocol that
+	// is not supported by this implementation.
+	InferencePoolReasonUnsupportedProtocol InferencePoolReason = "UnsupportedProtocol"
 )
