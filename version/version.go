6d9787bafc7edccb0524fd178242944aacfc9cdf
Bob Tian <bobzetian@google.com>
2025-07-25 19:20:27 -0700

    Update CRD BundleVersion to main-dev. (#1216)

--- version/version.go
+++ version/version.go
@@ -30,5 +30,5 @@ const (
 	BundleVersionAnnotation = "inference.networking.k8s.io/bundle-version"
 
 	// BundleVersion is the value used for labeling the version of the gateway-api-inference-extension.
-	BundleVersion = "v1.0.0-dev"
+	BundleVersion = "main-dev"
 )
