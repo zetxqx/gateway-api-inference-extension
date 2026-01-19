6c3256f98c9e9d2b5585d35375e5ed40904dd8ee
Nir Rozenbaum <nirro@il.ibm.com>
2026-01-19 15:11:14 +0200

    update bbr label filtering to align with the group of other resources. (#2178)
    This is aligned with k8s best practices.
    
    Signed-off-by: Nir Rozenbaum <nirro@il.ibm.com>

--- cmd/bbr/runner/runner.go
+++ cmd/bbr/runner/runner.go
@@ -115,13 +115,13 @@ func (r *Runner) Run(ctx context.Context) error {
 			return nil
 		}(),
 	}
-	// label "inference-gateway.k8s.io/managed" = "true" is used for server-side filtering of configmaps.
+	// label "inference.networking.k8s.io/bbr-managed" = "true" is used for server-side filtering of configmaps.
 	// only the configmap objects with this label will be tracked by bbr.
 	cacheOptions := cache.Options{
 		ByObject: map[client.Object]cache.ByObject{
 			&corev1.ConfigMap{}: {
 				Label: labels.SelectorFromSet(labels.Set{
-					"inference-gateway.k8s.io/managed": "true",
+					"inference.networking.k8s.io/bbr-managed": "true",
 				}),
 			},
 		},
