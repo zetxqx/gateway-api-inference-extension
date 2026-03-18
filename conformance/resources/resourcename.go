d9d2f39c39637daf778e586105784bc2cb214f7d
Daneyon Hansen <daneyon.hansen@solo.io>
2026-03-18 11:40:31 -0700

    test(conformance): cover InferencePool appProtocol semantics (#2574)
    * test(conformance): add inferencepool appProtocol coverage
    
    Signed-off-by: Daneyon Hansen <daneyon.hansen@solo.io>
    
    * test(conformance): isolate appProtocol fixtures
    
    Signed-off-by: Daneyon Hansen <daneyon.hansen@solo.io>
    
    * fix(conformance): use staging EPP image
    
    Signed-off-by: Daneyon Hansen <daneyon.hansen@solo.io>
    
    ---------
    
    Signed-off-by: Daneyon Hansen <daneyon.hansen@solo.io>

--- conformance/resources/resourcename.go
+++ conformance/resources/resourcename.go
@@ -24,13 +24,17 @@ const (
 	PrimaryGatewayName   = "conformance-primary"
 	SecondaryGatewayName = "conformance-secondary"
 
-	PrimaryInferencePoolName   = "primary-inference-pool"
-	SecondaryInferencePoolName = "secondary-inference-pool"
+	PrimaryInferencePoolName         = "primary-inference-pool"
+	SecondaryInferencePoolName       = "secondary-inference-pool"
+	AppProtocolHTTPInferencePoolName = "appprotocol-http-inference-pool"
+	AppProtocolH2CInferencePoolName  = "appprotocol-h2c-inference-pool"
 
-	PrimaryModelServerAppLabel         = "primary-inference-model-server"
-	SecondaryModelServerAppLabel       = "secondary-inference-model-server"
-	PrimaryModelServerDeploymentName   = "primary-inference-model-server-deployment"
-	SecondaryModelServerDeploymentName = "secondary-inference-model-server-deployment"
+	PrimaryModelServerAppLabel           = "primary-inference-model-server"
+	SecondaryModelServerAppLabel         = "secondary-inference-model-server"
+	AppProtocolModelServerAppLabel       = "appprotocol-inference-model-server"
+	PrimaryModelServerDeploymentName     = "primary-inference-model-server-deployment"
+	SecondaryModelServerDeploymentName   = "secondary-inference-model-server-deployment"
+	AppProtocolModelServerDeploymentName = "appprotocol-inference-model-server-deployment"
 
 	ModelServerPodReplicas    = 3
 	EndPointPickerPodReplicas = 1
@@ -40,8 +44,10 @@ var (
 	PrimaryGatewayNN   = types.NamespacedName{Name: PrimaryGatewayName, Namespace: InfraNamespace}
 	SecondaryGatewayNN = types.NamespacedName{Name: SecondaryGatewayName, Namespace: InfraNamespace}
 
-	PrimaryInferencePoolNN   = types.NamespacedName{Name: PrimaryInferencePoolName, Namespace: AppBackendNamespace}
-	SecondaryInferencePoolNN = types.NamespacedName{Name: SecondaryInferencePoolName, Namespace: AppBackendNamespace}
+	PrimaryInferencePoolNN         = types.NamespacedName{Name: PrimaryInferencePoolName, Namespace: AppBackendNamespace}
+	SecondaryInferencePoolNN       = types.NamespacedName{Name: SecondaryInferencePoolName, Namespace: AppBackendNamespace}
+	AppProtocolHTTPInferencePoolNN = types.NamespacedName{Name: AppProtocolHTTPInferencePoolName, Namespace: AppBackendNamespace}
+	AppProtocolH2CInferencePoolNN  = types.NamespacedName{Name: AppProtocolH2CInferencePoolName, Namespace: AppBackendNamespace}
 
 	PrimaryEppDeploymentNN   = types.NamespacedName{Name: "primary-app-endpoint-picker", Namespace: AppBackendNamespace}
 	SecondaryEppDeploymentNN = types.NamespacedName{Name: "secondary-app-endpoint-picker", Namespace: AppBackendNamespace}
